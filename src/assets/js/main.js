5b0fd52c487ff416f6bd2d05b45862621a1f24ae
Phil Wareham <phil@designhive.com>
2013-10-05 18:34:38 +0100

    Further work on unified header and footer

---
+++ src/assets/js/main.js
@@ -0,0 +1,192 @@
+(function ()
+{
+    'use strict';
+
+    requirejs.config({
+        paths:
+        {
+            'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min',
+            'jqueryui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min',
+            'webfont' : '//ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont',
+            'flowplayer': 'flowplayer/flowplayer.min'
+        },
+        shim:
+        {
+            'details': ['jquery'],
+            'placeholder': ['jquery'],
+            'flowplayer': ['jquery'],
+            'cookie': ['jquery'],
+            'jqueryui': ['jquery']
+        }
+    });
+
+    define('modernizr', function ()
+    {
+        return window.Modernizr;
+    });
+
+    require(['jquery'], function ($)
+    {
+        var details = $('details'), placeholder = $('textarea[placeholder], input[placeholder]'), code = $('pre code');
+
+        // Details polyfill.
+
+        if (details.length)
+        {
+            require(['details'], function ()
+            {
+                details.details();
+                $('html').addClass($.fn.details.support ? 'details' : 'no-details');
+            });
+        }
+
+        // Placeholder polyfill.
+
+        if (placeholder.length)
+        {
+            require(['placeholder'], function ()
+            {
+                placeholder.placeholder();
+            });
+        }
+
+        // Syntax highlighting.
+
+        if (code.length)
+        {
+            code.parent().addClass('prettyprint');
+
+            require(['prettify'], function ()
+            {
+                prettyPrint();
+            });
+        }
+    });
+
+    // Hack-fix for the iOS orientationchange zoom bug (NOTE: fixed in iOS 6).
+
+    require(['jquery', 'modernizr'], function ($, Modernizr)
+    {
+        if (Modernizr.touch)
+        {
+            var meta = $('meta[name=viewport]'), scales = [1, 1], fix = function ()
+            {
+                meta.attr('content', 'width=device-width,minimum-scale=' + scales[0] + ',maximum-scale=' + scales[1]);
+            };
+
+            fix();
+            scales = [0.25, 1.6];
+            $(document).one('gesturestart', fix);
+        }
+    });
+
+    // Test for SVG support via Modernizr, if yes then replace PNGs with SVGs.
+
+    require(['jquery', 'modernizr'], function ($, Modernizr)
+    {
+        if (Modernizr.svg)
+        {
+            $('img.svg').attr('src', function ()
+            {
+                return $(this).attr('src').replace('.png', '.svg');
+            });
+        }
+    });
+
+    // Responsive navigation.
+
+    require(['responsivenav'], function ()
+    {
+        responsiveNav('.nav-collapse', {
+            animate: true,       // Boolean: Use CSS3 transitions, true or false
+            transition: 400,     // Integer: Speed of the transition, in milliseconds
+            label: "Menu",       // String: Label for the navigation toggle
+            insert: "before",    // String: Insert the toggle before or after the navigation
+            customToggle: "",    // Selector: Specify the ID of a custom toggle
+            openPos: "relative", // String: Position of the opened nav, relative or static
+            jsClass: "js"        // String: 'JS enabled' class which is added to <html> el
+        });
+    });
+
+    // EU-cookie disclaimer.
+
+    require(['jquery', 'cookie'], function ($)
+    {
+        if (!$.cookie('acceptedCookies'))
+        {
+            var disclaimer = $('<aside id="cookie-disclaimer"><div class="container"><p><strong>This website uses cookies to enhance your experience.</strong> By continuing to use this website you agree to cookies being placed on your computer. If you wish to use this website but do not wish for cookies to be placed on your computer you can change the settings in your internet browser. <a href="#" data-action="close">Close</a>.</p></div></aside>');
+            $('body').prepend(disclaimer);
+            $.cookie('acceptedCookies', 1, {expires: 1461});
+
+            disclaimer.find('a').on('click', function (e)
+            {
+                e.preventDefault();
+
+                disclaimer.slideUp('fast', function ()
+                {
+                    disclaimer.remove();
+                });
+            });
+        }
+    });
+
+    // Flowplayer.
+
+    require(['jquery'], function ($)
+    {
+        var player = $('.videoplayer');
+
+        if (player.length)
+        {
+            require(['flowplayer'], function ()
+            {
+                player.flowplayer({
+                    splash: true,
+                    ratio: 0.417
+                });
+            });
+        }
+    });
+
+    // Fonts.
+
+    require(['webfont'], function ()
+    {
+        WebFont.load({
+            google:
+            {
+                families: ['PT+Serif:400,700,400italic,700italic:latin', 'Cousine::latin']
+            }
+        });
+    });
+
+    // Google+ '+1' button.
+
+    require(['jquery'], function ($)
+    {
+        if ($('.g-plusone').length)
+        {
+            require(['//apis.google.com/js/plusone.js']);
+        }
+    });
+
+    // Twitter 'Tweet' button.
+
+    require(['jquery'], function ($)
+    {
+        if ($('.twitter-share-button').length)
+        {
+            require(['//platform.twitter.com/widgets.js']);
+        }
+    });
+
+    // Analytics.
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-xxxxxxxx-x']);
+    _gaq.push(['_setDomainName', 'none']);
+    _gaq.push(['_gat._anonymizeIp']);
+    _gaq.push(['_trackPageview']);
+    require(['//www.google-analytics.com/ga.js']);
+
+})();
