9bb296882ea262859909fcf9780d45d6b8c0801d
Jukka Svahn <web@rahinaa.biz>
2013-11-14 13:42:43 +0200

    Travis tests Compass.

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,5 +139,5 @@ module.exports = function (grunt) {
     grunt.registerTask('sass', ['compass', 'cssmin', 'copy:css']);
     grunt.registerTask('default', ['watch']);
     grunt.registerTask('build', ['jshint', 'sass', 'copy:img', 'copy:js', 'uglify']);
-    grunt.registerTask('travis', ['jshint']);
+    grunt.registerTask('travis', ['jshint', 'compass']);
 };
