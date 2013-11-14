17781d489786799a8bb2ad8a72a5b6ed9e261757
Phil Wareham <phil@designhive.com>
2013-11-14 17:36:27 +0000

    Single quotemarks for Responsive Nav options

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -100,11 +100,11 @@
         responsiveNav('.nav-collapse', {
             animate: true,       // Boolean: Use CSS3 transitions, true or false
             transition: 400,     // Integer: Speed of the transition, in milliseconds
-            label: "Menu",       // String: Label for the navigation toggle
-            insert: "before",    // String: Insert the toggle before or after the navigation
-            customToggle: "",    // Selector: Specify the ID of a custom toggle
-            openPos: "relative", // String: Position of the opened nav, relative or static
-            jsClass: "js"        // String: 'JS enabled' class which is added to <html> el
+            label: 'Menu',       // String: Label for the navigation toggle
+            insert: 'before',    // String: Insert the toggle before or after the navigation
+            customToggle: '',    // Selector: Specify the ID of a custom toggle
+            openPos: 'relative', // String: Position of the opened nav, relative or static
+            jsClass: 'js'        // String: 'JS enabled' class which is added to <html> el
         });
     });
 
