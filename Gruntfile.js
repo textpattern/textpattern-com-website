37021d24081b37c9d0618fb091139f8ea8f4da87
Phil Wareham <phil@designhive.com>
2017-09-18 09:21:05 +0100

    CreateJS is now available via NPM

--- Gruntfile.js
+++ Gruntfile.js
@@ -252,6 +252,7 @@ module.exports = function (grunt)
                 files: [
                     {
                         '<%= paths.dest.js %>main.js': ['<%= paths.dest.js %>main.js'],
+                        '<%= paths.dest.js %>createjs.js': ['node_modules/createjs/builds/createjs.js'],
                         '<%= paths.dest.js %>galite.js': ['node_modules/ga-lite/src/ga-lite.js'],
                         '<%= paths.dest.js %>indexcanvas.js': ['<%= paths.src.js %>indexcanvas.js'],
                         '<%= paths.dest.js %>lunr.js': ['node_modules/lunr/lunr.js'],
