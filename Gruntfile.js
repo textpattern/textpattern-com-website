52a6fdede0ac1eff0753189856bb77ad1cd85ae5
Phil Wareham <phil@designhive.com>
2017-09-29 12:45:01 +0100

    Update humans.txt

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,8 +46,8 @@ module.exports = function (grunt)
         // Run some tasks in parallel to speed up the build process.
         concurrent: {
             dist: [
-                'css',
                 'copy',
+                'css',
                 'jshint'
             ]
         },
