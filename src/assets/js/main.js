d996de0ab04b3f66f321a06e315b48d284198985
Phil Wareham <phil@designhive.com>
2018-02-26 17:29:45 +0000

    Fetch GitHub API via JavaScript
    Works around our PHP version on server being older than supported TLS1.2.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,7 +9,8 @@
     // Load objects as variables.
 
     var code = document.querySelectorAll('pre code'),
-        navmenu = document.getElementById('site-navigation');
+        navmenu = document.getElementById('site-navigation'),
+        starbubble = document.getElementById('starbubble');
 
     // Syntax highlighting, via 'Prism'.
     // Applies syntax highlighting to `code` HTML elements.
@@ -29,6 +30,49 @@
             e.preventDefault();
             navtoggle.classList.toggle('site-navigation-toggle-active');
             navmenu.classList.toggle('site-navigation-open');
+        });
+    }
+
+    // GitHub star count.
+
+    if (starbubble) {
+        var githubStars = function (callback) {
+        	var xmlhttp = new XMLHttpRequest(),
+        		url = ['https://api.github.com/repos/textpattern/textpattern'],
+        		useCallback = (typeof(callback) === 'function');
+
+        	function countStars(response) {
+        		if (!(response instanceof Array)) {
+        			response = [response];
+        		}
+        		var stars = 0;
+        		for (var i in response) {
+                    if (response.hasOwnProperty(i)) {
+                        stars += parseInt(response[i].stargazers_count);
+                    }
+                }
+        		return stars;
+        	}
+
+        	if (useCallback) {
+        		xmlhttp.onreadystatechange = function() {
+        			if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+        				callback(countStars(JSON.parse(xmlhttp.responseText)));
+        			}
+        		};
+        	}
+
+        	xmlhttp.open('GET', url, useCallback);
+        	xmlhttp.setRequestHeader('Accept', 'application/vnd.github.v3+json');
+        	xmlhttp.send();
+
+        	if (!useCallback) {
+        		return countStars(JSON.parse(xmlhttp.responseText));
+        	}
+        };
+
+        githubStars(function(stars) {
+            starbubble.innerHTML = '<a id="starcount" class="count-bubble" rel="external" href="https://github.com/textpattern/textpattern/stargazers" title="Stargazers on GitHub">'+stars+'</a>';
         });
     }
 
