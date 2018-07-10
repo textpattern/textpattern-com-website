10b4a3c5b638d6fc30688c833b0d42cef85c0fa6
Phil Wareham <phil@designhive.com>
2018-07-10 10:07:57 +0100

    Build

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,9 @@ module.exports = function (grunt)
 {
     'use strict';
 
+    // Define Sass implmentation (i.e. Node or Dart).
+    const sass = require('node-sass');
+
     // Load all Grunt tasks.
     require('load-grunt-tasks')(grunt);
 
@@ -120,9 +123,10 @@ module.exports = function (grunt)
         jshint: {
             options: {
                 bitwise: true,
+                browser: true,
                 curly: true,
                 eqeqeq: true,
-                esversion: 5,
+                esversion: 6,
                 forin: true,
                 globals: {
                     define: true,
@@ -134,11 +138,9 @@ module.exports = function (grunt)
                 latedef: true,
                 noarg: true,
                 nonew: true,
-                quotmark: 'single',
-                undef: true,
-                unused: false,
                 strict: false,
-                browser: true
+                undef: true,
+                unused: false
             },
             files: [
                 'Gruntfile.js',
@@ -202,6 +204,7 @@ module.exports = function (grunt)
         // Sass configuration.
         sass: {
             options: {
+                implementation: sass,
                 outputStyle: 'expanded', // outputStyle = expanded, nested, compact or compressed.
                 sourceMap: false
             },
