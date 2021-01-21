cc88c6b4d4c473771642cba22cf89bc89c8005a5
philwareham <phil@designhive.com>
2021-01-21 16:41:07 +0000

    Sticky header on back-scroll
    Fixes #326

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -103,3 +103,47 @@
     isDark.addListener(toggleDarkMode);
 
 })();
+
+// Hide/show header on scroll
+
+(function(document, window) {
+	'use strict';
+
+	var element = document.getElementById('dynamic-header');
+
+	if (!element) {
+        return true;
+    }
+
+	var elHeight		= 0,
+		elTop			= 0,
+		dHeight			= 0,
+		wHeight			= 0,
+		wScrollCurrent	= 0,
+		wScrollBefore	= 0,
+		wScrollDiff		= 0;
+
+	window.addEventListener('scroll', function() {
+		elHeight		= element.offsetHeight;
+		dHeight			= document.body.offsetHeight;
+		wHeight			= window.innerHeight;
+		wScrollCurrent	= window.pageYOffset;
+		wScrollDiff		= wScrollBefore - wScrollCurrent;
+		elTop			= parseInt(window.getComputedStyle(element).getPropertyValue('top')) + wScrollDiff;
+
+		if( wScrollCurrent <= 0 ) {
+			element.style.top = '0px';
+        } else if (wScrollDiff > 0) {
+			element.style.top = (elTop > 0 ? 0 : elTop) + 'px';
+        } else if (wScrollDiff < 0) {
+			if (wScrollCurrent + wHeight >= dHeight - elHeight) {
+				element.style.top = ((elTop = wScrollCurrent + wHeight - dHeight ) < 0 ? elTop : 0) + 'px';
+            } else {
+				element.style.top = (Math.abs(elTop) > elHeight ? - elHeight : elTop) + 'px';
+            }
+		}
+
+		wScrollBefore = wScrollCurrent;
+	});
+
+}(document, window, 0));
