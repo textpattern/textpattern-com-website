b8cebd05ec859310d6b0a0ecbdaeec7aacec9b2b
philwareham <phil@designhive.com>
2021-02-12 13:49:11 +0000

    Save a HTTP request
    Can hash this in CSP when we tighten our rules.

--- Gruntfile.js
+++ Gruntfile.js
@@ -219,7 +219,6 @@ module.exports = function (grunt)
             dist: {
                 files: [
                     {
-                        '<%= paths.dest.js %>hi.js': ['<%= paths.src.js %>hi.js'],
                         '<%= paths.dest.js %>app.js': [
                             // Prism: Core.
                             'node_modules/prismjs/prism.js',
--- src/assets/js/hi.js
+++
@@ -1,1 +0,0 @@
-document.documentElement.className = 'js';
